724f3c525db8d19f5dd653009049d28e041a554a
N-isith <nisithalakshan94@gmail.com>
2024-04-26 11:07:41 +0530

    added route comments

--- UserBackend/routes/userRoutes.js
+++ UserBackend/routes/userRoutes.js
@@ -1,14 +1,19 @@
 const router = require("express").Router();
 const UserObject = require("../controllers/userController");
 
+//create route
 router.post('/user/create', UserObject.userCreate);
 
+//read route
 router.get('/user/read', UserObject.userRead);
 
+//get one user details route
 router.get('/user/get/:userId', UserObject.userGet);
 
+//update route
 router.put('/user/update/:userId', UserObject.userUpdate);
 
+//delete route
 router.delete('/user/delete/:userId', UserObject.userDelete);
 
 module.exports = router;

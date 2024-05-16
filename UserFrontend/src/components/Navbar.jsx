df8bdc29f4ddf248dcc4d2d818abc146f4bfd1cd
N-isith <nisithalakshan94@gmail.com>
2024-05-16 23:55:43 +0530

    navbar started

--- UserFrontend/src/components/Navbar.jsx
+++ UserFrontend/src/components/Navbar.jsx
@@ -4,14 +4,19 @@ export default function Navbar() {
   return (
     <div>
       <div>
-        <div>
-          <img src="" alt="logo" />
+        <div className="py-4 text-center text-emerald-800 font-extrabold text-5xl">
+          <h1>𝓟𝓘𝓒𝓣𝓞𝓡𝓘𝓐</h1>
         </div>
         <div>
-          <ul>
-            <li><a href="">Hi</a></li>
-            <li>huththi</li>
-            <li></li>
+          <ul className="flex justify-center gap-10 font-bold text-emerald-800">
+            <li className="bg-neutral-300 py-3 px-6 rounded-3xl"><a href="">Explore</a></li>
+            <li className="rounded-3xl">
+              <div>
+                <input type="search"  className="bg-neutral-300 rounded-3xl py-3 px-60" />Search
+              </div>
+            </li>
+            <li className="bg-neutral-300 py-3 px-6 rounded-3xl"><a href="">Join Us</a></li>
+            <li className="bg-emerald-800 py-3 px-6 rounded-3xl text-neutral-100"><a href="">Log In</a></li>
           </ul>
         </div>
       </div>

e338d90f0cf528e36dd80f06edbda27aa3644c78
N-isith <nisithalakshan94@gmail.com>
2024-06-18 23:22:45 +0530

    update operation halway done

--- UserFrontend/src/components/ProfileUser.jsx
+++ UserFrontend/src/components/ProfileUser.jsx
@@ -1,19 +1,14 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { MdOutlineModeEdit } from "react-icons/md";
-import { useNavigate } from "react-router-dom";
+import { MdFileDownloadDone } from "react-icons/md";
+import { useNavigate, useParams } from "react-router-dom";
 
 export default function ProfileUser() {
 
-    const userId = useState();
     const { navigate } = useNavigate();
 
     const [users, setUsers] = useState([]);
-    const [isNameDisabled, setIsNameDisabled] = useState(true);
-    const [isEmailDisabled, setIsEmailDisabled] = useState(true);
-    const [isPhoneDisabled, setIsPhoneDisabled] = useState(true);
-    const [isGenderDisabled, setIsGenderDisabled] = useState(true);
-
     useEffect(() => {
         function ReadUsers() {
             axios.get("http://localhost:4600/user/read")
@@ -37,6 +32,55 @@ export default function ProfileUser() {
                 alert("Deletion Failed!", err);
             })
     }
+
+
+
+
+    const userId = useParams();
+    const [userName, setUpdateUsername] = useState();
+    const [userEmail, setUpdateUseremail] = useState();
+    const [userPhone, setUpdateUserphone] = useState();
+    const [userGender, setUpdateUsergender] = useState();
+    const [userImage, setUpdateUserimage] = useState();
+
+    useEffect(() => {
+        axios.get(`http://localhost:4600/user/get/${userId}`)
+            .then((res) => {
+                setUpdateUsername(res.data);
+                setUpdateUseremail(res.data);
+                setUpdateUserphone(res.data);
+                setUpdateUsergender(res.data);
+                setUpdateUserimage(res.data);
+            })
+            .catch((err) => {
+                alert("user details fetch failed!", err);
+            })
+    }, [userId])
+
+    const SubmitUpdateUser = (u) => {
+        u.preventDefault();
+
+        const updateduser = {
+            userName,
+            userEmail,
+            userPhone,
+            userGender,
+            userImage,
+        }
+
+        axios.put(`http://localhost:4600/user/update/${userId}`, updateduser)
+            .then(() => {
+                alert("User updated successfully!");
+            })
+            .catch((err) => {
+                alert("User updation failed!", err);
+            })
+    }
+
+    const [isNameDisabled, setIsNameDisabled] = useState(true);
+    const [isEmailDisabled, setIsEmailDisabled] = useState(true);
+    const [isPhoneDisabled, setIsPhoneDisabled] = useState(true);
+    const [isGenderDisabled, setIsGenderDisabled] = useState(true);
 
     const enableEdit = (e, field) => {
         e.preventDefault();
@@ -73,7 +117,7 @@ export default function ProfileUser() {
                         }
                     </div>
 
-                    <form action="" className="w-3/5 px-10 py-5 text-neutral-200 text-start">
+                    <form action="" className="w-3/5 px-10 py-5 text-neutral-200 text-start" onSubmit={SubmitUpdateUser}>
                         <div className="mb-4">
                             <label htmlFor="" className="block mb-2 text-amber-300">Name: {" "}</label>
                             <input
@@ -81,11 +125,14 @@ export default function ProfileUser() {
                                 className="w-full h-8 outline outline-none bg-inherit border-b-2 ps-3"
                                 value={user.userName}
                                 disabled={isNameDisabled}
+                                onChange={(u) => {setUpdateUsername(u.target.value)}}
                             />
                             <button className="absolute -ml-6 mt-1" onClick={(e) => enableEdit(e, "name")}>
                                 <MdOutlineModeEdit className="size-5 text-amber-300" />
                             </button>
                         </div>
+
+
                         <div className="mb-4">
                             <label htmlFor="" className="block mb-2 text-amber-300">Email: {" "}</label>
                             <input
@@ -93,11 +140,14 @@ export default function ProfileUser() {
                                 className="w-full h-8 outline outline-none bg-inherit border-b-2 ps-3"
                                 value={user.userEmail}
                                 disabled={isEmailDisabled}
+                                onChange={(u) => {setUpdateUseremail(u.target.value)}}
                             />
                             <button className="absolute -ml-6 mt-1" onClick={(e) => enableEdit(e, "email")}>
                                 <MdOutlineModeEdit className="size-5 text-amber-300" />
                             </button>
                         </div>
+
+
                         <div className="mb-4">
                             <label htmlFor="" className="block mb-2 text-amber-300">Phone: {" "}</label>
                             <input
@@ -105,11 +155,14 @@ export default function ProfileUser() {
                                 className="w-full h-8 outline outline-none bg-inherit border-b-2 ps-3"
                                 value={user.userPhone}
                                 disabled={isPhoneDisabled}
+                                onChange={(u) => {setUpdateUserphone(u.target.value)}}
                             />
                             <button className="absolute -ml-6 mt-1" onClick={(e) => enableEdit(e, "phone")}>
                                 <MdOutlineModeEdit className="size-5 text-amber-300" />
                             </button>
                         </div>
+
+
                         <div className="mb-4">
                             <label htmlFor="" className="block mb-2 text-amber-300">Gender: {" "}</label>
                             <input
@@ -117,16 +170,24 @@ export default function ProfileUser() {
                                 className="w-full h-8 outline outline-none bg-inherit border-b-2 ps-3"
                                 value={user.userGender}
                                 disabled={isGenderDisabled}
+                                onChange={(u) => {setUpdateUsergender(u.target.value)}}
                             />
                             <button className="absolute -ml-6 mt-1" onClick={(e) => enableEdit(e, "gender")}>
                                 <MdOutlineModeEdit className="size-5 text-amber-300" />
                             </button>
                         </div>
-                        <button
-                            className="flex justify-center my-4 mt-7 ml-auto rounded-md py-1.5 px-4 text-md text-red-500 font-semibold bg-slate-700 hover:outline outline-1 hover:outline-amber-300"
-                            onClick={() => (DeleteUser(user._id))}>
-                            Delete your profile
-                        </button>
+
+
+                        <div className="flex justify-between">
+                            <button type="submit" className="mt-3" >
+                                <MdFileDownloadDone  className="size-10 text-amber-300"/>
+                            </button>
+                            <button
+                                className="flex justify-center my-4 mt-7 ml-auto rounded-md py-1.5 px-4 text-md text-red-500 font-semibold bg-slate-700 hover:outline outline-1 hover:outline-amber-300"
+                                onClick={() => (DeleteUser(user._id))}>
+                                Delete your profile
+                            </button>
+                        </div>
                     </form>
                 </div>
 

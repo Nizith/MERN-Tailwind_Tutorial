6271249aa41ecb565b02238cbab2139b755c0208
N-isith <nisithalakshan94@gmail.com>
2024-06-18 19:08:33 +0530

    allow edit when button click function done

--- UserFrontend/src/components/ProfileUser.jsx
+++ UserFrontend/src/components/ProfileUser.jsx
@@ -1,9 +1,14 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { MdOutlineModeEdit } from "react-icons/md";
 
 export default function ProfileUser() {
 
     const [users, setUsers] = useState([]);
+    const [isNameDisabled, setIsNameDisabled] = useState(true);
+    const [isEmailDisabled, setIsEmailDisabled] = useState(true);
+    const [isPhoneDisabled, setIsPhoneDisabled] = useState(true);
+    const [isGenderDisabled, setIsGenderDisabled] = useState(true);
 
     useEffect(() => {
         function ReadUsers() {
@@ -18,54 +23,88 @@ export default function ProfileUser() {
         ReadUsers();
     }, []);
 
-
+    const enableEdit = (e, field) => {
+        e.preventDefault();
+        switch (field) {
+            case "name":
+                setIsNameDisabled(false);
+                break;
+            case "email":
+                setIsEmailDisabled(false);
+                break;
+            case "phone":
+                setIsPhoneDisabled(false);
+                break;
+            case "gender":
+                setIsGenderDisabled(false);
+                break;
+            default:
+                break;
+        }
+    };
 
     return (
         <div className="m-auto w-screen">
             {users.map((user) => (
-                <div className="w-3/5 m-auto bg-slate-800 rounded-xl flex justify-between">
+                <div key={user._id} className="w-3/5 m-auto bg-slate-800 rounded-xl flex justify-between">
 
                     <div className="w-2/5 flex justify-center my-auto">
                         {user.userImage &&
 
                             <img src={`http://localhost:4600/user/uploads/${user.userImage}`}
-                                className="h-96 rounded-full"
+                                className="h-96 rounded-2xl"
                                 alt="User image" />
                         }
                     </div>
 
                     <form action="" className="w-3/5 px-10 py-5 text-neutral-200 text-start">
                         <div className="mb-4">
-                            <label htmlFor="" className="block mb-2 text-amber-300">Name: </label>
+                            <label htmlFor="" className="block mb-2 text-amber-300">Name: {" "}</label>
                             <input
                                 type="text"
-                                className="w-full h-8 outline outline-none bg-inherit border-b-2 ps-3 focus:bg-slate-900"
+                                className="w-full h-8 outline outline-none bg-inherit border-b-2 ps-3"
                                 value={user.userName}
+                                disabled={isNameDisabled}
                             />
+                            <button className="absolute -ml-6 mt-1" onClick={(e) => enableEdit(e, "name")}>
+                                <MdOutlineModeEdit className="size-5 text-amber-300" />
+                            </button>
                         </div>
                         <div className="mb-4">
-                            <label htmlFor="" className="block mb-2 text-amber-300">Email: </label>
+                            <label htmlFor="" className="block mb-2 text-amber-300">Email: {" "}</label>
                             <input
                                 type="text"
-                                className="w-full h-8 outline outline-none bg-inherit border-b-2 ps-3 focus:bg-slate-900"
+                                className="w-full h-8 outline outline-none bg-inherit border-b-2 ps-3 2"
                                 value={user.userEmail}
+                                disabled={isEmailDisabled}
                             />
+                            <button className="absolute -ml-6 mt-1" onClick={(e) => enableEdit(e, "email")}>
+                                <MdOutlineModeEdit className="size-5 text-amber-300" />
+                            </button>
                         </div>
                         <div className="mb-4">
-                            <label htmlFor="" className="block mb-2 text-amber-300">Phone:</label>
+                            <label htmlFor="" className="block mb-2 text-amber-300">Phone: {" "}</label>
                             <input
                                 type="text"
-                                className="w-full h-8 outline outline-none bg-inherit border-b-2 ps-3 focus:bg-slate-900"
+                                className="w-full h-8 outline outline-none bg-inherit border-b-2 ps-3 2"
                                 value={user.userPhone}
+                                disabled={isPhoneDisabled}
                             />
+                            <button className="absolute -ml-6 mt-1" onClick={(e) => enableEdit(e, "phone")}>
+                                <MdOutlineModeEdit className="size-5 text-amber-300" />
+                            </button>
                         </div>
                         <div className="mb-4">
-                            <label htmlFor="" className="block mb-2 text-amber-300">Gender:</label>
+                            <label htmlFor="" className="block mb-2 text-amber-300">Gender: {" "}</label>
                             <input
                                 type="text"
-                                className="w-full h-8 outline outline-none bg-inherit border-b-2 ps-3 focus:bg-slate-900"
+                                className="w-full h-8 outline outline-none bg-inherit border-b-2 ps-3"
                                 value={user.userGender}
+                                disabled={isGenderDisabled}
                             />
+                            <button className="absolute -ml-6 mt-1" onClick={(e) => enableEdit(e, "gender")}>
+                                <MdOutlineModeEdit className="size-5 text-amber-300" />
+                            </button>
                         </div>
                         <button type="submit" className="flex justify-center my-4 mt-7 ml-auto rounded-md py-1.5 px-4 text-md text-red-500 bg-slate-700 hover:outline outline-1 hover:outline-amber-300">Delete your profile</button>
                     </form>
